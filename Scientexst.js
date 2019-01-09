7c9a8457c6f13fd6be693fad4456d0505d9ccea4
MatthyPlayz <34276633+MatthyPlayz@users.noreply.github.com>
2019-01-09 16:41:11 -0500

    Update Scientexst.js

--- Scientexst.js
+++ Scientexst.js
@@ -16,25 +16,30 @@ G.AddData({
 			req:{'tribalism':true},
 			partOf:'essentials'
 		});
+	},
+	func:function()
+    {  
 		new G.Tech({
 			name:'Inventor machinery',
 			desc:'@unlocks machinery for [inventor]s, producing [creativity].',
 		});
+	},
+	func:function()
+    {  
 		new G.Unit({
 		name:'scientest',
 		desc:'@generates [insight] more frequently than a [dreamer]<>A [scientest] spends their time experimenting with different objects around them.',
 		icon:[0,0,'sheet'],
 		cost:{'food':50},
 		use:{'worker':1},
-		effects:[
-			{type:'gather',what:{'insight':0.3}},
-			{type:'gather',what:{'insight':0.15},
-			{type:'mult',value:1.2,req:{'wisdom rituals':'on'}}
-		],
+		effects:[{type:'gather',what:{'insight':0.3}},{type:'gather',what:{'insight':0.15}}],
 		category:'discovery',
 		req:{'construction':true},
 		limitPer:{'house':1},
 		});
+	},
+	func:function()
+    {  
 		new G.Unit({
 			name:'inventor',
 			desc:'@generates [things] more frequently than the [thing] that generates that.<>A [inventor] makes contraptions that makes things faster than the usual person.',
@@ -42,9 +47,7 @@ G.AddData({
 			req:{'tribalism':true},
 			cost:{'food':50},
 			use:{'worker':1},
-			effects:[
-				{type:'gather',what:{'fruit':0.3},req:{'creativity':5}}
-			],
+			effects:{type:'gather',what:{'fruit':0.3},req:{'creativity':5}},
 			category:'discovery',
 			req:{'construction':true},
 			limitPer:{'house':1},
